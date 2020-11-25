0eac22282e922a9102134c100940f208af9614f1
Max Shenfield <shenfieldmax@gmail.com>
2020-11-24 18:18:45 -0800

    Get audio working on mobile

--- index.js
+++ index.js
@@ -18,16 +18,28 @@ document.addEventListener("DOMContentLoaded", () => {
   moonMask.style.right = `${size * proportion}em`;
 
   const audio = document.querySelector("#background-audio");
+  // For mobile browsers, the `autoplay` attribute isn't honored, even if initially muted.
+  // Track whether the audio is actually playing so we can manually trigger a `play()` if needed.
+  // https://developer.mozilla.org/en-US/docs/Web/Media/Autoplay_guide
+  var isPlaying = false;
+  audio.addEventListener("play", () => isPlaying = true);
+
   const muteButton = document.querySelector("#mute-button");
   // Force initial muted state - Firefox remembers checkbox state otherwise
   muteButton.checked = true;
   muteButton.addEventListener("input", inputEvent => {
     inputEvent.preventDefault();
     audio.muted = inputEvent.target.checked;
+    // Manually play if not already.
+    if (!audio.muted && !isPlaying) {
+      audio.play();
+      isPlaying = true;
+    }
   });
+
   // Using the <audio> "loop" attribute results in a conspicuous gap between
-  // loops. To make the background noise as unobtrusive as possible, extra
-  // massaging to do a gap-less loop.
+  // loops. To make the background noise as unobtrusive as possible, do extra
+  // massaging for a gap-less loop.
   audio.addEventListener("timeupdate", () => {
     const proportionPlayed = audio.currentTime / audio.duration;
     if (proportionPlayed > .95) {
