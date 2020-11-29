4cf368f01bdd9ae4fdb8ecf2940d38e355503eea
Max Shenfield <shenfieldmax@gmail.com>
2020-11-28 17:39:09 -0800

    Add stars

--- index.js
+++ index.js
@@ -11,12 +11,40 @@ function fullnessOfMoon() {
   }
 }
 
+/* Place stars throughout an evenly spaced grid
+ *
+ * Used to generate the stars and then manually add them to the page.
+ */
+function generateStars(logOnly) {
+  // Spread stars evenly over a 2000x2000 grid. The absolute measurements mean
+  // they don't move around when the screen is resized, and the grid helps
+  // them be spread evenly.
+  const starContainer = document.querySelector(".star-container");
+  const starScape = { width: 2000, height: 2000, rows: 10, columns: 10 };
+  const colWidth = starScape.width / starScape.columns;
+  const rowHeight = starScape.height / starScape.rows;
+  for (var x = 0; x < starScape.columns ; x++) {
+    for (var y = 0; y < starScape.rows; y++) {
+      // Salt the x,y coords so they range within their column.
+      const xOffset = x * colWidth + Math.random() * colWidth;
+      const yOffset = y * rowHeight + Math.random() * rowHeight;
+      if (logOnly) {
+        console.log(`<div class="star" style="top: ${yOffset}px; left: ${xOffset}px;"></div>`);
+        continue;
+      }
+      const star = document.createElement("div");
+      star.classList.add("star");
+      star.style.left = `${xOffset}px`;
+      star.style.top = `${yOffset}px`;
+      starContainer.appendChild(star);
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const proportion = fullnessOfMoon();
-  console.log(proportion);
   const moonMask = document.querySelector(".moon-mask");
   const size = window.parseInt(window.getComputedStyle(moonMask).getPropertyValue("--moon-size"));
-  console.log(size * proportion);
   moonMask.style.right = `${size * proportion}em`;
 
   const audio = document.querySelector("#background-audio");
